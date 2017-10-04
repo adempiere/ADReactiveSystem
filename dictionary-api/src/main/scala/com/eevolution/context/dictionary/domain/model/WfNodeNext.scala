3d06a65f8b3a2c8cdd9b1c1930aa57066076cb7c
eEvolution Emeris Hernandez <emeris.hernandez@e-evolution.com>
2017-10-04 15:05:32 -0500

    adempiere/ADReactiveSystem#2 Starting Application Dictionary Reactive System https://github.com/adempiere/ADReactiveSystem/issues/2

---
+++ dictionary-api/src/main/scala/com/eevolution/context/dictionary/domain/model/WfNodeNext.scala
@@ -0,0 +1,88 @@
+package com.eevolution.context.dictionary.domain.model
+
+import ai.x.play.json.Jsonx
+import com.eevolution.context.dictionary.api.{ActiveEnabled, DomainModel, Identifiable, Traceable}
+import org.joda.time.DateTime
+
+/**
+  * Copyright (C) 2003-2017, e-Evolution Consultants S.A. , http://www.e-evolution.com
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  * Email: emeris.hernandez@e-evolution.com, http://www.e-evolution.com , http://github.com/e-Evolution
+  * Created by emeris.hernandez@e-evolution.com , www.e-evolution.com
+  */
+
+/**
+  * Wf Node Next Entity
+  * @param wfNodeNextId Wf Node Next ID
+  * @param isActive Is Active
+  * @param created Created
+  * @param createdBy Created By
+  * @param updated Updated
+  * @param updatedBy Updated By
+  * @param wfNodeId Wf Node ID
+  * @param wfNextId Wf Next ID
+  * @param description Description
+  * @param seqNo Seq No
+  * @param entityType Entity Type
+  * @param transitionCode Transition Code
+  * @param isStdUserWorkFlow Is Std User Work Flow
+  * @param uuId UU ID
+  */
+
+case class WfNodeNext(wfNodeNextId: Int,
+                      isActive: Boolean = true,
+                      created: DateTime = DateTime.now(),
+                      createdBy: Int,
+                      updated: DateTime = DateTime.now(),
+                      updatedBy: Int,
+                      wfNodeId: Int,
+                      wfNextId: Int,
+                      description: Option[String],
+                      seqNo: Int,
+                      entityType: String = "D",
+                      transitionCode: Option[String],
+                      isStdUserWorkFlow: Boolean = false,
+                      uuId: Option[String]
+                     ) extends DomainModel
+
+  with ActiveEnabled
+  with Identifiable
+  with Traceable {
+  override type ActiveEnabled = this.type
+  override type Identifiable = this.type
+  override type Traceable = this.type
+
+  override def Id: Int = wfNodeNextId
+
+  override val entityName: String = "AD_WfNodeNext"
+  override val identifier: String = "AD_WfNodeNext_ID"
+}
+
+object WfNodeNext {
+  implicit lazy val jsonFormat = Jsonx.formatCaseClass[WfNodeNext]
+  def create(wfNodeNextId: Int,
+             isActive: Boolean,
+             created: DateTime,
+             createdBy: Int,
+             updated: DateTime,
+             updatedBy: Int,
+             wfNodeId: Int,
+             wfNextId: Int,
+             description: String,
+             seqNo: Int,
+             entityType: String,
+             transitionCode: String,
+             isStdUserWorkFlow: Boolean,
+             uuId: String) = WfNodeNext(wfNodeNextId, isActive, created, createdBy, updated, updatedBy,
+    wfNodeId, wfNextId, None, seqNo, entityType, None, isStdUserWorkFlow, None)
+
+}

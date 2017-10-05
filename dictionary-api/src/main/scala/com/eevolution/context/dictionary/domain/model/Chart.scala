ae6d4ff242fcb50c43b8332afa86251feef1db36
eEvolution Eduardo Moreno <eduardo.moreno@e-evolution.com>
2017-10-05 14:32:13 -0500

    adempiere/ADReactiveSystem#2 Starting Application Dictionary Reactive System https://github.com/adempiere/ADReactiveSystem/issues/2

---
+++ dictionary-api/src/main/scala/com/eevolution/context/dictionary/domain/model/Chart.scala
@@ -0,0 +1,97 @@
+package com.eevolution.context.dictionary.domain.model
+
+import ai.x.play.json.Jsonx
+import com.eevolution.context.dictionary.api.{ActiveEnabled, DomainModel, Identifiable, Traceable}
+import org.joda.time.DateTime
+/**
+  * Copyright (C) 2003-2017, e-Evolution Consultants S.A. , http://www.e-evolution.com
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  * Email: eduardo.moreno@e-evolution.com, http://www.e-evolution.com , http://github.com/e-Evolution
+  * Created by eduardo.moreno@e-evolution.com , www.e-evolution.com
+  */
+/**
+  * Chart Entity
+  * @param chartId Chart ID
+  * @param chartType Chart Type
+  * @param created Created
+  * @param createdBy Created By
+  * @param description Description
+  * @param isActive Is Active
+  * @param name Name
+  * @param updated Updated
+  * @param updatedBy Updated By
+  * @param winHeight Win Height
+  * @param isDisplayLegend Is Display Legend
+  * @param chartOrientation Chart Orientation
+  * @param isTimeSeries Is Time Series
+  * @param timeUnit Time Unit
+  * @param timeScope Time Scope
+  * @param domainLabel Domain Label
+  * @param rageLabel Range Label
+  * @param uuId UU ID
+  */
+
+case class Chart (chartId: Int,
+                  chartType: String,
+                  created: DateTime = DateTime.now,
+                  createdBy: Int,
+                  description: String,
+                  isActive: Boolean,
+                  name: String,
+                  updated: DateTime = DateTime.now,
+                  updatedBy: Int,
+                  winHeight: Option[Int],
+                  isDisplayLegend: Boolean = true,
+                  chartOrientation: String = "V",
+                  isTimeSeries: Boolean = false,
+                  timeUnit: Option[Boolean],
+                  timeScope: Option[Int],
+                  domainLabel: Option[String],
+                  rageLabel: Option[String],
+                  uuId: Option[String]
+                 ) extends DomainModel
+
+  with ActiveEnabled
+  with Identifiable
+  with Traceable {
+  override type ActiveEnabled = this.type
+  override type Identifiable = this.type
+  override type Traceable = this.type
+  override def Id: Int = chartId
+
+  override val entityName: String = "AD_Chart"
+  override val identifier: String = "AD_Chart_ID"
+
+}
+
+object Chart {
+  implicit lazy val jsonFormat = Jsonx.formatCaseClass[Chart]
+  def create(chartId: Int,
+             chartType: String,
+             created: DateTime,
+             createdBy: Int,
+             description: String,
+             isActive: Boolean,
+             name: String,
+             updated: DateTime,
+             updatedBy: Int,
+             winHeight: Int,
+             isDisplayLegend: Boolean,
+             chartOrientation: String,
+             isTimeSeries: Boolean,
+             timeUnit: Boolean,
+             timeScope: Int,
+             domainLabel: String,
+             rageLabel: String,
+             uuId: String) = Chart(chartId, chartType, created, createdBy, description, isActive, name, updated,
+    updatedBy, None, isDisplayLegend, chartOrientation, isTimeSeries, None, None, None, None, None)
+}
---
+++ dictionary-api/src/main/scala/com/eevolution/context/dictionary/domain/model/ChartDataSource.scala
@@ -0,0 +1,102 @@
+package com.eevolution.context.dictionary.domain.model
+
+import ai.x.play.json.Jsonx
+import com.eevolution.context.dictionary.api.{ActiveEnabled, DomainModel, Identifiable, Traceable}
+import org.joda.time.DateTime
+/**
+  * Copyright (C) 2003-2017, e-Evolution Consultants S.A. , http://www.e-evolution.com
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  * Email: eduardo.moreno@e-evolution.com, http://www.e-evolution.com , http://github.com/e-Evolution
+  * Created by eduardo.moreno@e-evolution.com , www.e-evolution.com
+  */
+/**
+  * Chart Date Source Entity
+  * @param chartDataSourceId Chart Data Source ID
+  * @param chartId Chart ID
+  * @param tableId Table ID
+  * @param categoryColumn Category Column
+  * @param created Created
+  * @param createdBy Created By
+  * @param dateColumn Date Column
+  * @param description Description
+  * @param entityType Entity Type
+  * @param fromClause From Clause
+  * @param isActive Is Active
+  * @param keyColumn Key Column
+  * @param Name Name
+  * @param updated Updated
+  * @param updatedBy Updated By
+  * @param valueColumn Value Column
+  * @param whereClause Where Clause
+  * @param timeOffSet Time Off Set
+  * @param seriesColumn Series Column
+  * @param uuId UU ID
+  */
+case class ChartDataSource (chartDataSourceId: Int,
+                            chartId: Int,
+                            tableId: Option[Int],
+                            categoryColumn: Option[String],
+                            created: DateTime = DateTime.now,
+                            createdBy: Int,
+                            dateColumn: Option[String],
+                            description: Option[String],
+                            entityType: String = "D",
+                            fromClause: String,
+                            isActive: Boolean = true,
+                            keyColumn: Option[String],
+                            Name: String,
+                            updated: DateTime = DateTime.now,
+                            updatedBy: Int,
+                            valueColumn: String,
+                            whereClause: String,
+                            timeOffSet: Option[Int],
+                            seriesColumn: Option[String],
+                            uuId: Option[String]
+                           ) extends DomainModel
+
+  with ActiveEnabled
+  with Identifiable
+  with Traceable {
+  override type ActiveEnabled = this.type
+  override type Identifiable = this.type
+  override type Traceable = this.type
+  override def Id: Int = chartDataSourceId
+
+  override val entityName: String = "AD_ChartDataSource"
+  override val identifier: String = "AD_ChartDataSource_ID"
+
+}
+
+object ChartDataSource {
+  implicit lazy val jsonFormat = Jsonx.formatCaseClass[ChartDataSource]
+  def create(chartDataSourceId: Int,
+             chartId: Int,
+             tableId: Int,
+             categoryColumn: String,
+             created:  DateTime,
+             createdBy: Int,
+             dateColumn: String,
+             description: String,
+             entityType: String,
+             fromClause: String,
+             isActive: Boolean,
+             keyColumn: String,
+             Name: String,
+             updated:  DateTime,
+             updatedBy: Int,
+             valueColumn: String,
+             whereClause: String,
+             timeOffSet:Int,
+             seriesColumn: String,
+             uuId: String) = ChartDataSource(chartDataSourceId, chartId, None, None, created, createdBy, None,
+    None, entityType, fromClause, isActive, None, Name, updated, updatedBy, valueColumn, whereClause, None, None, None)
+}

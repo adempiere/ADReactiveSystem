f3c17da60ee15c9d61ef91a35ff69fb3454d0975
eEvolution Eduardo Moreno <eduardo.moreno@e-evolution.com>
2017-10-13 15:58:46 -0500

    Merge remote-tracking branch 'origin/master'

---
+++ dictionary-api/src/main/scala/com/eevolution/context/dictionary/domain/model/TaskAccess.scala
@@ -0,0 +1,76 @@
+package com.eevolution.context.dictionary.domain.model
+
+import ai.x.play.json.Jsonx
+import com.eevolution.context.dictionary.api.{ActiveEnabled, DomainModel, Identifiable, Traceable}
+import org.joda.time.DateTime
+/**
+  * Copyright (C) 2003-2017, e-Evolution Consultants S.A. , http://www.e-evolution.com
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  * Email: eduardo.moreno@e-evolution.com, http://www.e-evolution.com , http://github.com/e-Evolution
+  * Created by eduardo.moreno@e-evolution.com , www.e-evolution.com
+  */
+/**
+  * Task Entity
+  * @param taskId Task ID
+  * @param roleId Role ID
+  * @param tenantId Tenant ID
+  * @param organizationId Organization ID
+  * @param isActive Is Active
+  * @param created Created
+  * @param createdBy Created By
+  * @param updated Updated
+  * @param updatedBy Updated By
+  * @param isReadWrite Is Read Write
+  * @param uuId UUID
+  */
+
+case class TaskAccess (taskId: Int,
+                       roleId: Int,
+                       tenantId: Int,
+                       organizationId: Int,
+                       isActive: Boolean = true,
+                       created: DateTime = DateTime.now,
+                       createdBy: Int,
+                       updated: DateTime = DateTime.now,
+                       updatedBy: Int,
+                       isReadWrite: Boolean = true,
+                       uuId: Option[String]
+                      ) extends DomainModel
+
+  with ActiveEnabled
+  with Identifiable
+  with Traceable {
+  override type ActiveEnabled = this.type
+  override type Identifiable = this.type
+  override type Traceable = this.type
+  override def Id: Int = 0
+
+  override val entityName: String = "AD_Package_ImpDetail"
+  override val identifier: String = null
+
+}
+
+object TaskAccess {
+  implicit lazy val jsonFormat = Jsonx.formatCaseClass[TaskAccess]
+  def create(taskId: Int,
+             roleId: Int,
+             tenantId: Int,
+             organizationId: Int,
+             isActive: Boolean,
+             created: DateTime,
+             createdBy: Int,
+             updated: DateTime,
+             updatedBy: Int,
+             isReadWrite: Boolean,
+             uuId: String) = TaskAccess(taskId, roleId, tenantId, organizationId, isActive, created,
+    createdBy, updated, updatedBy, isReadWrite, None)
+}

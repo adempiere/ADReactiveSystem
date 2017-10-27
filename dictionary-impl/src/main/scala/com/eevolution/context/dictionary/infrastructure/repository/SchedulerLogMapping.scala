9fa7b606a32538136a1c2df27c51b409d14a628d
eEvolution Eduardo Moreno <eduardo.moreno@e-evolution.com>
2017-10-27 13:15:44 -0500

    Merge remote-tracking branch 'origin/master'

---
+++ dictionary-impl/src/main/scala/com/eevolution/context/dictionary/infrastructure/repository/SchedulerLogMapping.scala
@@ -0,0 +1,45 @@
+package com.eevolution.context.dictionary.infrastructure.repository
+
+import com.eevolution.context.dictionary.domain.model.SchedulerLog
+import com.eevolution.context.dictionary.infrastructure.db.DbContext._
+
+/**
+  * Copyright (C) 2003-2017, e-Evolution Consultants S.A. , http://www.e-evolution.com
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  * Email: eduardo.moreno@e-evolution.com, http://www.e-evolution.com , http://github.com/e-Evolution
+  * Created by eduardo.moreno@e-evolution.com , www.e-evolution.com
+  */
+
+/**
+  * Scheduler Mapping
+  */
+trait SchedulerLogMapping  {
+  val querySchedulerLog = quote {
+    querySchema[SchedulerLog]("AD_SchedulerLog",
+      _.schedulerId -> "AD_Scheduler_ID",
+      _.schedulerLogId -> "AD_SchedulerLog_ID",
+      _.tenantId -> "AD_Client_ID",
+      _.organizationId-> "AD_Org_ID",
+      _.isActive -> "IsActive",
+      _.created -> "Created",
+      _.createdBy -> "CreatedBy",
+      _.updated -> "Updated",
+      _.updatedBy -> "UpdatedBy",
+      _.isError -> "IsError",
+      _.summary -> "Summary",
+      _.reference -> "Reference",
+      _.description -> "Description",
+      _.textMsg -> "TextMsg",
+      _.binaryData -> "BinaryData",
+      _.uuid -> "UUID")
+  }
+}

7ea43e913c4db9a921672c41776777980b2f3c30
eEvolution Emeris Hernandez <emeris.hernandez@e-evolution.com>
2017-11-01 16:30:15 -0600

    ADReactiveSystem#10 Implement Repository for each Domain Model #10 https://github.com/adempiere/ADReactiveSystem/issues/10

---
+++ dictionary-api/src/main/scala/com/eevolution/context/dictionary/domain/api/repository/FieldRepository.scala
@@ -0,0 +1,6 @@
+package com.eevolution.context.dictionary.domain.api.repository
+
+import com.eevolution.context.dictionary._
+
+trait FieldRepository [Field , Int] extends api.Repostory [Field , Int] {
+}

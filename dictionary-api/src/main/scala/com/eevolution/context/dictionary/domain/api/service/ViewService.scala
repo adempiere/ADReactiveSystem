60b4315eb6d92f9b57dc24cad17e9e01a93bdf0f
eEvolution Eduardo Moreno <eduardo.moreno@e-evolution.com>
2017-11-13 10:41:21 -0600

    ADReactiveSystem#11 Implement Service for each Domain Model #11 https://github.com/adempiere/ADReactiveSystem/issues/11

---
+++ dictionary-api/src/main/scala/com/eevolution/context/dictionary/domain/api/service/ViewService.scala
@@ -0,0 +1,28 @@
+package com.eevolution.context.dictionary.domain.api.service
+
+import com.eevolution.context.dictionary.api
+import com.eevolution.context.dictionary.domain.model.View
+
+/**
+  * Copyright (C) 2003-2017, e-Evolution Consultants S.A. , http://www.e-evolution.com
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  * Email: eduardo.moreno@e-evolution.com, http://www.e-evolution.com , http://github.com/e-Evolution
+  * Created by eduardo.moreno@e-evolution.com , www.e-evolution.com
+  */
+
+/**
+  * View Service
+  */
+
+trait ViewService extends api.Service[View, Int] {
+  //Definition
+}
